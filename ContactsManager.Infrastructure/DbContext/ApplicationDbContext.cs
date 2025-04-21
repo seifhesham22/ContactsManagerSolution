bba833d921a8aa18712a0e7eefbc063fad90638b
Seif Elmoazen <seifhesham882@gmail.com>
2025-04-21 18:52:45 +0700

    Created Identity Models.

--- ContactsManager.Infrastructure/DbContext/ApplicationDbContext.cs
+++ ContactsManager.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using ContactsManager.Core.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser , ApplicationRole , Guid>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
